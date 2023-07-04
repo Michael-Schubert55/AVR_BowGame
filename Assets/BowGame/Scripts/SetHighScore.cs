0b29d8bf5e304003d99e8560926acfe3174a77d8
Paul Kunisch <paul.kunisch@gmail.com>
2023-07-04 18:18:14 +0200

    +SetHighScore

--- Assets/BowGame/Scripts/SetHighScore.cs
+++ Assets/BowGame/Scripts/SetHighScore.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.SocialPlatforms.Impl;
 
 public class SetHighScore : MonoBehaviour
@@ -9,19 +10,31 @@ public class SetHighScore : MonoBehaviour
     [SerializeField]
     private TMP_Text highscore;
 
+    private string currentSceneName;
+
+
     // Start is called before the first frame update
     void Awake()
     {
-        //Debug.Log("S")
-        highscore.text = "Level             |    High score   |    Fewest arrows used\n"+
-                         "Übungslevel:        "+PlayerPrefs.GetInt("Score Level_1")+"                           " + PlayerPrefs.GetInt("Arrows Level_1")+"\n"+
-                         "Level_1:                "+PlayerPrefs.GetInt("Score Level_2")+"                           " + PlayerPrefs.GetInt("Arrows Level_2")+"\n" +
-                         "Level_2:               "+PlayerPrefs.GetInt("Score Level_3")+"                           "+ PlayerPrefs.GetInt("Arrows Level_3");
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //if (!PlayerPrefs.HasKey("Score " + currentSceneName)) && !(PlayerPrefs.HasKey("Arrows " + currentSceneName)))
+        if (PlayerPrefs.HasKey("Score Level_1") && PlayerPrefs.HasKey("Score Level_2") && (PlayerPrefs.HasKey("Score Level_3")))
+        //if (!(PlayerPrefs.HasKey("Score Level_1")) && !(PlayerPrefs.HasKey("Score Level_2")) && (!(PlayerPrefs.HasKey("Score Level_3"))))
+            {
+            highscore.text = "  Level            |         Hoechste Punktzahl        |       Wenigste Pfeile\n\n\n" +
+                         "Übungslevel:                      "+PlayerPrefs.GetInt("Score Level_1")+"                                              "+PlayerPrefs.GetInt("Arrows Level_1")+"\n" +
+                         "Level_1:                              "+PlayerPrefs.GetInt("Score Level_2")+"                                             "+PlayerPrefs.GetInt("Arrows Level_2") + "\n" +
+                         "Level_2:                             "+PlayerPrefs.GetInt("Score Level_3")+"                                             "+PlayerPrefs.GetInt("Arrows Level_3");
+        }else
+        {
+            highscore.text = "  Level                |    Hoechste Punktzahl           |     Wenigste Pfeile\n\n\n" +
+                         "Übungslevel:        "+"Keine Punkte vorhanden"+"            "+"Keine Anzahl vorhanden"+"\n" +
+                         "Level_1:             "+"Keine Punkte vorhanden"+"            "+"Keine Anzahl vorhanden"+"\n" +
+                         "Level_2:            "+"Keine Punkte vorhanden"+"            "+"Keine Anzahl vorhanden";
+        }
+
+
     }
+    
 }
+
